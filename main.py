ba0a4114028007b3a91f140e9450b3441c2b131d
Alexey Uvarov <alexejuv@yandex.ru>
2022-02-04 19:39:18 +0300

    Add bug but at least scatters

--- main.py
+++ main.py
@@ -6,26 +6,27 @@ from scipy import integrate, optimize
 def scatter_between_level_surfaces(x0: np.array, foo, levels,
                                    starting_direction=np.array([1, 0]),
                                    n_jumps=10):
-    jump_points = np.zeros((x0.shape[0], n_jumps))
+    jump_points = np.zeros((x0.shape[0], n_jumps + 1))
+    jump_points[:, 0] = x0
     current_direction = starting_direction
     current_point = x0
     for i in range(n_jumps):
-        t = np.infty
-        for level in levels:
-            t_new = find_t_to_level(current_point, current_direction, foo, level)
-            if t_new < t:
-                t = t_new
+        if i % 2 == 0:
+            level = max(levels)
+        else:
+            level = min(levels)
+        t = find_t_to_level(current_point, current_direction, foo, level)
         current_point = current_point + t * current_direction
         current_direction = next_direction(current_direction)
-        jump_points[:, i] = current_point
+        jump_points[:, i+1] = current_point
 
     return jump_points
 
 
 def find_t_to_level(x0, direction, foo, level):
     directional_difference = difference_along_path(x0, direction, foo, level)
-    # t = optimize.broyden1(directional_difference, 0)
-    t = optimize.newton(directional_difference, 1e-8)
+    t = optimize.broyden1(directional_difference, 0)
+    # t = optimize.newton(directional_difference, 1e-8)
     return t
 
 
@@ -45,16 +46,22 @@ def next_direction(direction):
 if __name__ == "__main__":
 
     def f(x):
-        return x[0]**2 + x[1]**2
+            return x[0] * x[1]
 
 
     target_levels = [2, 3]
     x0 = np.array([0, 1])
     direction = np.array([1, 0])
 
-    t = find_t_to_level(x0, direction, f, 2)
-    print(t)
-    print(x0 + t * direction)
+    # t = find_t_to_level(x0, direction, f, 2)
+    # print(t)
+    # print(x0 + t * direction)
     jump_points = scatter_between_level_surfaces(x0, f, target_levels)
     print(jump_points)
+
+    # xs = np.linspace(0.1, 20, num=101)
+    # plt.plot(xs, 2 / xs)
+    # plt.plot(xs, 3 / xs)
+    plt.plot([x0[0]] + jump_points[0, :], [x0[1]] + jump_points[1, :])
+    plt.show()
 
